6511eb8f515b89c39776481bc949aa94932aa1a7
520RPazos <158420056+520RPazos@users.noreply.github.com>
2024-02-04 21:49:42 -0700

    Update cis129_lab02_shortStory.py
    Short rhyming story from 5 print statements  and comments giving detail on the code

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello World!')
+# Author: Rene Pazos
+# Lab2 in my CIS129 Course
+# Python prints simple story about a man from Nantucket
+
+print('There once was a man from Nantucket')
+print('He fell down a well and guess what he found')
+print('Nothing in the well other than a bucket')
+print('The fireman came and saved the man.')
+print('Knowing he was just a lowly wireman')
